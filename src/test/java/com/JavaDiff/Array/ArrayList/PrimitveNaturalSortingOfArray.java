c63a913da76679cf12a455ea87af789baac863af
Tanmay13 <Tanmaykhope77@gmail.com>
2020-02-11 01:47:47 +0530

    PrimitveNaturalSortingOfArray make changes in code

--- src/test/java/com/JavaDiff/Array/ArrayList/PrimitveNaturalSortingOfArray.java
+++ src/test/java/com/JavaDiff/Array/ArrayList/PrimitveNaturalSortingOfArray.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 public class PrimitveNaturalSortingOfArray {
 	
 	   public static void main(String[] args) 
-	   {
+	      {
+		   
+		   System.out.println("First Difference Interview questions");
 	      Integer[] intArrays = {31, 83, 53, 97, 29, 7, 13,  47, 79};
 	      String[] strArrays = {"Karthi", "Vikram", "Vijay","Simbhu", "Suriya", "Ajith"};
 	      System.out.println("Before sorting: Integer Arrays\n");
